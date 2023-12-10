c8c00ccfb5c227e586b4f8d65ff03337f5131910
sangwanharsh2003 <hsangwanalt2002@gmail.com>
2023-12-10 11:59:23 +0530

    added

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: "https://cyan-brainy-cape-buffalo.cyclic.app//api/",
+    baseURL: "https://cyan-brainy-cape-buffalo.cyclic.app/api/",
     withCredentials: true,
 });
 
