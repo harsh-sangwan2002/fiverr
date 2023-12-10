470f703e99a98beb8c54ff9074c85e3287a0c2d5
sangwanharsh2003 <hsangwanalt2002@gmail.com>
2023-12-10 09:47:37 +0530

    added

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -2,6 +2,31 @@ import Order from "../models/order.model.js";
 import Gig from "../models/gig.model.js";
 import Stripe from "stripe";
 
+export const createOrder = async (req, res, next) => {
+
+    const gig = await Gig.findById(req.params.gigId);
+
+    const newOrder = new Order({
+        gigId: gig._id,
+        img: gig.cover,
+        title: gig.title,
+        buyerId: req.userId,
+        sellerId: gig.userId,
+        price: gig.price,
+        payment_intent: "temporary",
+    });
+
+    try {
+
+        await newOrder.save();
+        res.status(200).send("Successful.")
+
+    } catch (err) {
+        next(err);
+    }
+
+}
+
 export const intent = async (req, res, next) => {
     const stripe = new Stripe(process.env.STRIPE);
 
