a3cc41fe5a750a683683be71f0408e3bdfa45c58
sangwanharsh2003 <hsangwanalt2002@gmail.com>
2023-12-11 09:48:32 +0530

    added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,8 +45,7 @@ export const login = async (req, res, next) => {
         const { password, ...info } = user._doc;
 
         res.cookie("accessToken", token, {
-            httpOnly: false,
-            secure: true,
+            httpOnly: true,
             path: "/",
             maxAge: 30 * 24 * 60 * 60 * 1000,
         }).status(200).send(info);
